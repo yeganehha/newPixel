0f3377c2099046e2b2823eebda4b52f682289ed6
thekashfi <alikashfi12345@gmail.com>
2022-08-31 15:27:30 +0430

    charts 10%

--- app/Http/Controllers/Panel/DashboardController.php
+++ app/Http/Controllers/Panel/DashboardController.php
@@ -4,12 +4,31 @@ namespace App\Http\Controllers\Panel;
 
 use App\Http\Controllers\Controller;
 use RestCord\DiscordClient;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public static function getUsers()
     {
+        $dateFrom = Carbon::now()->subDays(30);
+        $dateTo = Carbon::now();
 
+        $result = User::select('users.*', DB::raw('count(id) as `count`'),  DB::raw('MONTH(created_at) month, DAY(created_at) day'))
+            ->whereBetween('users.created_at', [$dateFrom, $dateTo])
+            ->groupby('month', 'day')
+            ->orderBy('month')
+            ->orderBy('day')
+            ->get();
+        dd($result);
+
+        
+        // $return = array_fill();
+
+        // return User::selectRaw('year(created_at) year, monthname(created_at) month, count(*) data')
+        //     ->groupBy('year', 'month')
+        //     ->get();
     }
 
     public static function getRoles(){
